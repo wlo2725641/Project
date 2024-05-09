98c9979c0ae3eb536c8b723afc7884e1ee9fa103
wlo2725641 <jason7813734@yhaoo.com.tw>
2024-05-09 20:46:13 +0800

    5/9

--- js/swiperPages.js
+++ js/swiperPages.js
@@ -1,15 +1,4 @@
 var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 50,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-var swiper2 = new Swiper(".mySwiper2", {
-  direction: "vertical",
-  spaceBetween: 50,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  effect: "cards",
+  grabCursor: true,
 });
